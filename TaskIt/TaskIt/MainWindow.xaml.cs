c01bba0532d61090d2411c47843c0c47a10caffc
Carlos José Mecha <carlosjm893@gmail.com>
2023-02-15 13:50:06 +0100

    Añadido método Window_MouseMove(), para poder hacer la ventana "arrastrable" cuando esta maximizada.

--- TaskIt/TaskIt/MainWindow.xaml.cs
+++ TaskIt/TaskIt/MainWindow.xaml.cs
@@ -15,43 +15,61 @@ using System.Windows.Shapes;
 
 namespace TaskIt
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+   /// <summary>
+   /// Lógica de interacción para MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+         MouseMove += Window_MouseMove;
+      }
 
-        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                this.DragMove();
-            }
-        }
+      private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+      {
+         if (e.ChangedButton == MouseButton.Left)
+         {
+            this.DragMove();
+         }
+      }
 
-        private void closeButton_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
+      private void Window_MouseMove(object sender, MouseEventArgs e)
+      {
+         if (e.LeftButton == MouseButtonState.Pressed && WindowState == WindowState.Maximized)
+         {
+            double mouseX = e.GetPosition(this).X;
+            double windowHeight = ActualHeight;
+            double windowTop = e.GetPosition(this).Y;
+            double windowLeft = (mouseX / ActualWidth) * (SystemParameters.PrimaryScreenWidth - RestoreBounds.Width);
+            Top = windowTop - (windowHeight);
+            Left = windowLeft;
+            WindowState = WindowState.Normal;
+            DragMove();
+         }
+      }
 
-        private void Button_Maximize(object sender, RoutedEventArgs e)
-        {
-            if (this.WindowState == WindowState.Maximized)
-            {
-                this.WindowState = WindowState.Normal;
-            }
-            else
-            {
-                this.WindowState = WindowState.Maximized;
-            }
-        }
-        private void Button_Minimize(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
-    }
+
+
+      private void closeButton_Click(object sender, RoutedEventArgs e)
+      {
+         Close();
+      }
+
+      private void Button_Maximize(object sender, RoutedEventArgs e)
+      {
+         if (this.WindowState == WindowState.Maximized)
+         {
+            this.WindowState = WindowState.Normal;
+         }
+         else
+         {
+            this.WindowState = WindowState.Maximized;
+         }
+      }
+      private void Button_Minimize(object sender, RoutedEventArgs e)
+      {
+         this.WindowState = WindowState.Minimized;
+      }
+   }
 }
